7226c2e0136a50b478b345bd2f538e1ce7badd90
Ivan Udovin <wilcot@ya.ru>
2021-11-21 01:07:09 +0300

    Fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 test("renders app", () => {
-	render(<App />);
+	render(<BrowserRouter><App /></BrowserRouter>);
 	const linkElement = screen.getByText(/Online Judge/i);
 	expect(linkElement).toBeInTheDocument();
 });
