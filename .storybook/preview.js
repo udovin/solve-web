e821de5938d702b431c9494dbd3e6f0d88b1ce72
Ivan Udovin <wilcot@ya.ru>
2023-01-02 20:37:07 +0300

    Update colors

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,29 @@
-export const parameters = {
-	actions: {
-		argTypesRegex: "^on[A-Z].*"
-	}
+import "../src/index.scss";
+
+export const globalTypes = {
+	theme: {
+		name: "Theme",
+		description: "Global theme for components",
+		defaultValue: "light",
+		toolbar: {
+			icon: "circlehollow",
+			// Array of plain string values or MenuItem shape (see below)
+			items: ["light", "dark"],
+			// Property that specifies if the name of the item will be displayed
+			showName: true,
+			// Change title based on selected value
+			dynamicTitle: true,
+		},
+	},
 };
+
+const withThemeProvider = (Story, context) => {
+	const theme = context.globals.theme;
+	return (
+		<div className={`theme-${theme}`}>
+			<Story />
+		</div>
+	)
+}
+
+export const decorators = [withThemeProvider];
