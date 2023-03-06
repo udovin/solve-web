4e8a78b8b139b217a0a919d8a03fa16e3ecb6f55
Ivan Udovin <wilcot@ya.ru>
2023-03-07 02:28:10 +0300

    Change styles

--- src/ui/Code/index.tsx
+++ src/ui/Code/index.tsx
@@ -1,11 +1,14 @@
 import { FC, useEffect, useRef } from "react";
 import { EditorView, lineNumbers } from "@codemirror/view";
 import { EditorState, Extension } from "@codemirror/state";
-import { defaultHighlightStyle, syntaxHighlighting, bracketMatching, foldGutter } from "@codemirror/language";
+import { HighlightStyle, syntaxHighlighting, bracketMatching, foldGutter } from "@codemirror/language";
+import { tags } from "@lezer/highlight";
 import { cpp } from "@codemirror/lang-cpp";
 import { python } from "@codemirror/lang-python";
 import { java } from "@codemirror/lang-java";
 import { rust } from "@codemirror/lang-rust";
+
+import "./index.scss";
 
 export type CodeProps = {
     className?: string;
@@ -22,6 +25,15 @@ const languages: Record<string, Extension | undefined> = {
     "rs": rust(),
 };
 
+const highlightStyle = HighlightStyle.define([
+    { tag: tags.comment, class: "hl-comment" },
+    { tag: tags.keyword, class: "hl-keyword" },
+    { tag: tags.processingInstruction, class: "hl-keyword" },
+    { tag: tags.typeName, class: "hl-type" },
+    { tag: tags.string, class: "hl-string" },
+    { tag: tags.number, class: "hl-number" },
+]);
+
 const Code: FC<CodeProps> = props => {
     const { className, content, language, editable, readOnly } = props;
     const ref = useRef<HTMLDivElement>(null);
@@ -33,7 +45,7 @@ const Code: FC<CodeProps> = props => {
             [
                 lineNumbers(),
                 foldGutter(),
-                syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
+                syntaxHighlighting(highlightStyle, { fallback: true }),
                 bracketMatching(),
             ],
         ];
@@ -56,7 +68,7 @@ const Code: FC<CodeProps> = props => {
         });
         return () => view?.destroy();
     }, [ref, content, language, editable, readOnly]);
-    return <div className={className} ref={ref} />;
+    return <div className={`ui-code ${className ?? ""}`} ref={ref} />;
 };
 
 export default Code;
