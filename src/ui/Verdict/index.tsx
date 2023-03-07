e1442ddd17301834f5c6b8477058f6abe76e18af
Ivan Udovin <wilcot@ya.ru>
2023-03-08 02:55:05 +0300

    Add support for points

--- src/ui/Verdict/index.tsx
+++ src/ui/Verdict/index.tsx
@@ -103,18 +103,23 @@ const getTitle = (info?: VerdictInfo, testNumber?: number) => {
 const Verdict: FC<VerdictProps> = props => {
     const { report } = props;
     const verdict = report?.verdict;
+    const points = report?.points;
     const used_time = report?.used_time;
     const used_memory = report?.used_memory;
     const test_number = report?.test_number;
     const info = verdict ? VERDICTS[verdict] : VERDICTS["running"];
     const title = getTitle(info, test_number) ?? verdict;
     const disableUsage = info?.disableUsage ?? false;
-    return <Tooltip className={`ui-verdict ${info?.code ?? "unknown"}`} content={<div className="ui-verdict-details">
+    return <Tooltip className="ui-verdict-wrap" content={<div className="ui-verdict-details">
         <span className={`item description ui-verdict ${info?.code ?? "unknown"}`}>{info?.description ?? verdict}</span>
+        {points !== undefined && <span className="item points">Points: {points}</span>}
         {test_number && <span className="item test">on test {test_number}</span>}
         {!disableUsage && <span className="item time"><Duration value={(used_time ?? 0) * 0.001} /></span>}
         {!disableUsage && <span className="item memory"><ByteSize value={used_memory ?? 0} /></span>}
-    </div>}>{title}</Tooltip >;
+    </div>}>
+        <span className={`ui-verdict ${info?.code ?? "unknown"}`}>{title}</span>
+        {points !== undefined && <span className="points">Points: {points}</span>}
+    </Tooltip >;
 };
 
 type TestVerdictProps = {
