1ca2f083b8b052cc61636f50f6a5e6f660e210f4
Ivan Udovin <wilcot@ya.ru>
2024-08-06 03:33:06 +0300

    Add tooltip for datetime

--- src/ui/DateTime/index.tsx
+++ src/ui/DateTime/index.tsx
@@ -1,6 +1,7 @@
 import { FC, useContext, useEffect, useMemo, useState } from "react";
 import { DateFormatter } from "../../utils";
 import { LocaleContext } from "../Locale";
+import Tooltip from "../Tooltip";
 
 export type DateTimeProps = {
 	value: number;
@@ -21,32 +22,33 @@ const DateTime: FC<DateTimeProps> = props => {
 		let intervalID = setInterval(() => setNow(getNow()), 1000);
 		return () => clearInterval(intervalID);
 	}, [setNow]);
+	const formatted = format(new Date(value * 1000));
 	if (value > now - 1 && value < now + 1) {
-		return localizeKey("datetime_now", "now");
+		return <Tooltip content={formatted}>{localizeKey("datetime_now", "now")}</Tooltip>;
 	} else if (value <= now) {
 		const seconds = now - value;
 		const minutes = Math.trunc(seconds / 60)
 		const hours = Math.trunc(minutes / 60);
 		if (seconds < 60) {
-			return localizePluralKey("datetime_past_second", ["%d second ago", "%d seconds ago"], seconds);
+			return <Tooltip content={formatted}>{localizePluralKey("datetime_past_second", ["%d second ago", "%d seconds ago"], seconds)}</Tooltip>;
 		} else if (minutes < 60) {
-			return localizePluralKey("datetime_past_minute", ["%d minute ago", "%d minutes ago"], minutes);
+			return <Tooltip content={formatted}>{localizePluralKey("datetime_past_minute", ["%d minute ago", "%d minutes ago"], minutes)}</Tooltip>;
 		} else if (hours < 24) {
-			return localizePluralKey("datetime_past_hour", ["%d hour ago", "%d hours ago"], hours);
+			return <Tooltip content={formatted}>{localizePluralKey("datetime_past_hour", ["%d hour ago", "%d hours ago"], hours)}</Tooltip>;
 		}
 	} else {
 		const seconds = value - now;
 		const minutes = Math.trunc(seconds / 60)
 		const hours = Math.trunc(minutes / 60);
 		if (seconds < 60) {
-			return localizePluralKey("datetime_future_second", ["in %d second", "in %d seconds"], seconds);
+			return <Tooltip content={formatted}>{localizePluralKey("datetime_future_second", ["in %d second", "in %d seconds"], seconds)}</Tooltip>;
 		} else if (minutes < 60) {
-			return localizePluralKey("datetime_future_minute", ["in %d minute", "in %d minutes"], minutes);
+			return <Tooltip content={formatted}>{localizePluralKey("datetime_future_minute", ["in %d minute", "in %d minutes"], minutes)}</Tooltip>;
 		} else if (hours < 24) {
-			return localizePluralKey("datetime_future_hour", ["in %d hour", "in %d hours"], hours);
+			return <Tooltip content={formatted}>{localizePluralKey("datetime_future_hour", ["in %d hour", "in %d hours"], hours)}</Tooltip>;
 		}
 	}
-	return format(new Date(value * 1000));
+	return formatted;
 };
 
 export default DateTime;
