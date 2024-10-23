90f0130f838a3fb73c4b54609053436bcfd9cfc0
Ivan Udovin <wilcot@ya.ru>
2024-10-24 02:31:33 +0300

    Fix lstlisting style

--- src/ui/Latex/index.stories.tsx
+++ src/ui/Latex/index.stories.tsx
@@ -1,13 +1,16 @@
-import { FC, useState } from "react";
-import Latex, { LatexProps } from ".";
+import Latex from ".";
+import Block from "../Block";
 
 export default {
 	title: "Latex",
 };
 
 export const Index = () => <>
-	<Latex content={`\\section{Fermat's Last Theorem} $a^n + b^n \\ne c^n$, $a, b, c, n \\in \\N$, $n \\ge 3$`} />
-	<Latex content={`a\\^{}b = $a^b$`} />
-	<Latex content={`\\^a, \\^{a}, \\^{}a`} />
-	<Latex content={`\\underline{Underline} {\\bfseries Bold} $\\bf{Bold2}$ {\\bf Bold3} Normal`} />
+	<Block>
+		<Latex content={`\\section{Fermat's Last Theorem} $a^n + b^n \\ne c^n$, $a, b, c, n \\in \\N$, $n \\ge 3$`} />
+		<Latex content={`a\\^{}b = $a^b$`} />
+		<Latex content={`\\^a, \\^{a}, \\^{}a`} />
+		<Latex content={`\\underline{Underline} {\\bfseries Bold} $\\bf{Bold2}$ {\\bf Bold3} Normal`} />
+		<Latex content={`\\begin{lstlisting}let a = "Hello, World!";\\end{lstlisting}`} />
+	</Block>
 </>;
